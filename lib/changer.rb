3417f49943060b4dded51a1ff7174d20ab796b00
Will Sherman <vvj5@icloud.com>
2015-06-29 09:45:12 -0500

    passing all tests

--- lib/changer.rb
+++ lib/changer.rb
@@ -1,5 +1,14 @@
 class Changer
   def self.run(amt)
-
-  end
+   coins = []
+   [25, 10, 5, 1].each do |number|
+      if amt >= number
+        i = (amt / number).floor
+        i.times { coins << [number] }
+        value = i * number
+        amt -= value
+      end
+    end
+   coins
+  end #run method
 end
--- spec/changer_test.rb
+++ spec/changer_test.rb
@@ -12,4 +12,17 @@ class ChangerTest < MiniTest::Test
     assert_respond_to(Changer, :run)
   end
 
+  def test_get_quarters_back
+    assert_equal([[25]], Changer.run(25))
+  end
+
+  def test_get_quarter_and_a_dime_back
+    assert_equal([[25], [10]], Changer.run(35))
+  end
+
+  def test_other_amts
+    assert_equal([[25], [10], [5], [1]], Changer.run(41))
+    assert_equal([[25], [25], [25], [5], [1], [1]], Changer.run(82))
+    assert_equal([[25], [25], [10], [5], [1]], Changer.run(66))
+  end
 end

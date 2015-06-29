a18d34c234ed16f76b3a08c869759156d86b87ab
Justin Herrick <justin@theironyard.com>
2015-06-29 08:34:22 -0500

    Initial Commit.

---
+++ lib/changer.rb
@@ -0,0 +1,5 @@
+class Changer
+  def self.run(amt)
+
+  end
+end
---
+++ spec/changer_test.rb
@@ -0,0 +1,15 @@
+require 'minitest/autorun'
+require 'minitest/pride'
+require_relative '../lib/changer'
+
+class ChangerTest < MiniTest::Test
+
+  def test_changer_exists
+    assert(Changer)
+  end
+
+  def test_run_works
+    assert_respond_to(Changer, :run)
+  end
+
+end
